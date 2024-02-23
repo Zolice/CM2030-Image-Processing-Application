1af5a5be1a6f0c56eb510af890ef1ec4f9c30118
Yong Zhen Long <vividzhenlong@gmail.com>
2024-02-23 12:04:09 +0800

    Initial Commit

---
+++ js/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    // Setup the p5.js Canvas
+    canvas = createCanvas(400, 400)
+
+    // Set the background colour
+    background(125);
+}
+
+function draw() {
+    background(125)
+}
