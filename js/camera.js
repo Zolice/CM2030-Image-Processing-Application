d7fbde3f371573ff88732d7a1fe798873021a992
Yong Zhen Long <vividzhenlong@gmail.com>
2024-02-27 16:03:19 +0800

    Fix for Camera.js code breaking

--- js/camera.js
+++ js/camera.js
@@ -211,16 +211,16 @@ class Camera {
                 let r = img.pixels[index]
                 let g = img.pixels[index + 1]
                 let b = img.pixels[index + 2]
-                let a = img.pixels[index + 3]
+                let a = img.pixels[index + 3];
 
                 // Red Filter
-                (x < cameraSize.x) ? img.pixels[index] = r : img.pixels[index] = 0
+                (x < cameraSize.x) ? img.pixels[index] = r : img.pixels[index] = 0;
 
                 // Green Filter
-                (x >= cameraSize.x && x <= cameraSize.x * 2) ? img.pixels[index + 1] = g : img.pixels[index + 1] = 0
+                (x >= cameraSize.x && x <= cameraSize.x * 2) ? img.pixels[index + 1] = g : img.pixels[index + 1] = 0;
 
                 // Blue Filter
-                (x >= cameraSize.x * 2) ? img.pixels[index + 2] = b : img.pixels[index + 2] = 0
+                (x >= cameraSize.x * 2) ? img.pixels[index + 2] = b : img.pixels[index + 2] = 0;
 
                 img.pixels[index + 3] = a
             }
